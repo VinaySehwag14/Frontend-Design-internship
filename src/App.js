05a290fe723d69c9168ffc5d7bd68071690b2a1f
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-03-04 17:31:53 +0530

    added subscibe component

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Download />
       <Testimonials />
       <Subscribe />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import "./subscribe.css";
 
 const Subscribe = () => {
-  return <div className="subscibe">Subscribe</div>;
+  return (
+    <div className="subscribe__container">
+      <div className="subscribe">
+        <h1>Subscribe !</h1>
+        <span>Join our newsletter to never miss any latest news.</span>
+      </div>
+      <div className="subscribe__mail">
+        <input type="email" placeholder="Enter your e-mail address here" />
+        <button className="subscribe__button">Join Us</button>
+      </div>
+    </div>
+  );
 };
 
 export default Subscribe;
